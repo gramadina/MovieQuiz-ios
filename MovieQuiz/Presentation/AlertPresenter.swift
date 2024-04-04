8ca12bb86ab653ae564d39b52ab8dcbe6f31ec03
Daria Granina <dashuny14@gmail.com>
2024-04-04 23:23:15 +0400

    Спринт 5

---
+++ MovieQuiz/Presentation/AlertPresenter.swift
@@ -0,0 +1,30 @@
+import UIKit
+
+protocol AlertPresenterProtocol {
+    func showAlert(quiz result: AlertModel)
+}
+
+class AlertPresenter: AlertPresenterProtocol {
+    
+    weak var alertDelegate: UIViewController?
+    
+    init(alertDelegate: UIViewController) {
+        self.alertDelegate = alertDelegate
+    }
+    
+    func showAlert(quiz result: AlertModel) {
+        let alert = UIAlertController(
+            title: result.title,
+            message: result.message,
+            preferredStyle: .alert)
+        
+        let action = UIAlertAction(
+            title: result.buttonText,
+            style: .default,
+            handler: result.completion)
+        
+        alert.addAction(action)
+        
+        alertDelegate?.present(alert, animated: true, completion: nil)
+    }
+}
