3ee9ce4d2783092c991cbdc86c74f2eebc6d390a
Daria Granina <dashuny14@gmail.com>
2024-04-10 20:06:55 +0400

    Added MoviesLoader and NetworkClient

---
+++ MovieQuiz/Models/MostPopularMovies.swift
@@ -0,0 +1,8 @@
+//
+//  MostPopularMovies.swift
+//  MovieQuiz
+//
+//  Created by Daria Granina on 10.04.2024.
+//
+
+import Foundation
---
+++ MovieQuiz/Services/MoviesLoader.swift
@@ -0,0 +1,8 @@
+//
+//  MoviesLoader.swift
+//  MovieQuiz
+//
+//  Created by Daria Granina on 10.04.2024.
+//
+
+import Foundation
