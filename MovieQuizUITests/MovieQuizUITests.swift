751943f3b4f20a08bb34a994bbdbf48f317563cb
Daria Granina <dashuny14@gmail.com>
2024-04-18 14:55:20 +0400

    Промежуточно надо закоммитить

---
+++ MovieQuizUITests/MovieQuizUITests.swift
@@ -0,0 +1,53 @@
+//
+//  MovieQuizUITests.swift
+//  MovieQuizUITests
+//
+//  Created by Daria Granina on 15.04.2024.
+//
+
+import XCTest
+
+final class MovieQuizUITests: XCTestCase {
+    
+    var app: XCUIApplication!
+    
+    override func setUpWithError() throws {
+        try super.setUpWithError()
+        
+        app = XCUIApplication()
+        app.launch()
+        
+        continueAfterFailure = false
+    }
+    
+    override func tearDownWithError() throws {
+        try super.tearDownWithError()
+        
+        app.terminate()
+        app = nil
+    }
+    
+    func testExample() throws {
+        
+        let app = XCUIApplication()
+        app.launch()
+        
+        
+    }
+    
+    func testYesButton() {
+        sleep(10)
+        
+        let firstPoster = app.images["Poster"]
+        let firstPosterData = firstPoster.screenshot().pngRepresentation
+        
+        app.buttons["Yes"].tap()
+        sleep(10)
+        
+        let secondPoster = app.images["Poster"]
+        let secondPosterData = secondPoster.screenshot().pngRepresentation
+        
+        XCTAssertNotEqual(firstPosterData, secondPosterData)
+    }
+    
+}
